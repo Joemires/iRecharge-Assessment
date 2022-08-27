fc4fed5c9b770f9f97e491deb34e3be9975b9ba1
sendlify <uiwehee@gmail.com>
2022-08-27 22:08:48 +0100

    fixed flutterwave keys

--- config/payment.php
+++ config/payment.php
@@ -2,8 +2,8 @@
 return [
     'gateway' => 'flutterwave',
     'flutterwave' => [
-        'secret_key' => 'FLWSECK_TEST-292d8452958fea900cd986f0ffd54e99-X',
-        'public_key' => 'FLWPUBK_TEST-864d63d9611afd7c9ffa5d5664eb3fe6-X',
-        'encryption_key' => 'FLWSECK_TEST1f8682798e3d'
+        'secret_key' => env('FW_SECKEY'),
+        'public_key' => env('FW_PUBKEY'),
+        'encryption_key' => env('FW_ENCKEY')
     ]
 ];
