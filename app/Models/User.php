c28893cb732fec661bd101e49d52fa727f61493b
sendlify <uiwehee@gmail.com>
2022-08-27 21:53:27 +0100

    added transaction helper to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,40 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get all of the transactions for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function transactions(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function deposit(Float|Int $amount, ?Array $meta, Bool $confirmed = true)
+    {
+        $this->wallet_balance += $amount;
+        $this->save();
+
+        return $this->transactions()->create([
+            'uuid' => Str::orderedUuid(),
+            'amount' => abs($amount),
+            'confirmed' => $confirmed,
+            'meta' => $meta
+        ]);
+    }
+
+    public function withdraw(Float|Int $amount, ?Array $meta, Bool $confirmed = true)
+    {
+        $this->wallet_balance -= $amount;
+        $this->save();
+
+        return $this->transactions()->create([
+            'uuid' => Str::orderedUuid(),
+            'amount' => -1 * abs($amount),
+            'confirmed' => $confirmed,
+            'meta' => $meta
+        ]);
+    }
 }
