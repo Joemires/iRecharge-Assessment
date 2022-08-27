8fa80b3c4852153620e47a0db99613ef297a35c7
sendlify <uiwehee@gmail.com>
2022-08-27 21:50:28 +0100

    setup transaction model and migrations

---
+++ app/Models/Transaction.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'uuid',
+        'amount',
+        'confirmed',
+        'meta'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    /**
+     * Get the user that owns the Transaction
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function confirm()
+    {
+        abort_if($this->confirmed, 501, 'Transaction already confirmed');
+        if($this->amount < 0) $this->user()->decrement('wallet_balance', abs($this->amount));
+        else $this->user()->increment('wallet_balance', abs($this->amount));
+
+        $this->confirmed = 1;
+        $this->save();
+
+    }
+}
---
+++ database/migrations/2022_08_25_185359_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->decimal("amount", 16, 2)->default(0);
+            $table->boolean('confirmed')->default(false);
+            $table->json('meta')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
